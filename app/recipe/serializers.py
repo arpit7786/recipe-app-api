cdd3f96c9480a87513a1af9e9f3b97c134addf89
arpit7786 <arpit97singh@gmail.com>
2023-10-30 10:14:58 +0530

    Completed implementation of Ingredients API.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,6 +6,7 @@ from rest_framework import serializers
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 
 
@@ -17,13 +18,23 @@ class TagSerializer(serializers.ModelSerializer):
         read_only_fields = ['id']
 
 
+class IngredientSerializer(serializers.ModelSerializer):
+    """ Serializer for ingredient. """
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """ Serializer for recipes. """
     tag = TagSerializer(many=True, required=False)
+    ingredient = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tag']
+        fields = ['id', 'title', 'time_minutes', 'price',
+                  'link', 'tag', 'ingredient']
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
@@ -36,20 +47,37 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tag.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """ Handle getting or creating ingredients as needed. """
+        auth_user = self.context['request'].user
+        for ingredients in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredients,
+            )
+            recipe.ingredient.add(ingredient_obj)
+
     def create(self, validated_data):
         """ Create a recipe. """
         tags = validated_data.pop('tag', [])
+        ingredients = validated_data.pop('ingredient', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
     def update(self, instance, validated_data):
         """ Update recipe. """
         tags = validated_data.pop('tag', [])
+        ingredients = validated_data.pop('ingredient', [])
         if tags is not None:
             instance.tag.clear()
             self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            instance.ingredient.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -38,14 +39,24 @@ class RecipeviewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.DestroyModelMixin, mixins.UpdateModelMixin,
-                 mixins.ListModelMixin, viewsets.GenericViewSet):
-    """ Manage Tags in the database. """
-    serializer_class = serializers.TagSerializer
-    queryset = Tag.objects.all()
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin, mixins.UpdateModelMixin,
+                            mixins.ListModelMixin, viewsets.GenericViewSet):
+    """ Base ViewSet for recipe attributes """
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """ Filter queryset for authenticated users. """
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """ Manage Tags in the database. """
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """ Manage ingredients in recipe. """
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
